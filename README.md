e44f6912b62a9f32ec2badc446734de18e71995f
Luke Spicknall <lukejspicknall@gmail.com>
2022-04-14 22:29:59 -0400

    initial commit. create calculator with html and css. start initial js math.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # calculator
+This example of a calculator is a project in Odin. This porject occurs at the 91% completion mark of Foundations.
---
+++ script.js
@@ -0,0 +1,28 @@
+let button = document.getElementsByClassName('.button');
+button.addEventListener('click', () => {
+    alert('hi')
+})
+
+function btnLink () {
+
+}
+
+function add(a, b){
+    return a + b;
+}
+
+function sub(a, b){
+    return a - b;
+}
+
+function mult(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function operate(){
+
+}
